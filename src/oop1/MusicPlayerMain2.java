45412b1871ab2df93cf798a1ad239c06869149a2
yuwankang <kangyuwan@naver.com>
2024-03-28 00:11:49 +0900

    절차지향 프로그래밍과 데이터 묶기

---
+++ src/oop1/MusicPlayerMain2.java
@@ -0,0 +1,35 @@
+package oop1;
+
+public class MusicPlayerMain2 {
+    public static void main(String[] args) {
+        MusicPlayerData data = new MusicPlayerData();
+
+        //음악 플레이어 켜기
+        data.isOn = true;
+        System.out.println("음악 플레이어를 시작합니다.");
+
+        //볼륨 증가
+        data.volume++;
+        System.out.println("음악 플레이어 볼륨:" + data.volume);
+
+        //볼륨 증가
+        data.volume++;
+        System.out.println("음악 플레이어 볼륨:"+data.volume);
+
+        //볼륨 감소
+        data.volume--;
+        System.out.println("음악 플레이어 볼륨:"+data.volume);
+
+        //음악 플레이어 상태
+        System.out.println("음악 플레이어 상태 확인");
+        if(data.isOn){
+            System.out.println("음악 플레이어 on, 볼륨:"+data.volume);
+        }else {
+            System.out.println("음악 플레이어 OFF");
+        }
+
+        data.isOn = false;
+        System.out.println("음악 플레이어 종료");
+    }
+}
+
