c01c91178afb389675739a486f09b48042dbc27e
yuwankang <kangyuwan@naver.com>
2024-04-01 23:14:38 +0900

    절차지향 프로그래밍 데이터 묶기 메서드 추출 후 클래스와 메서드를 사용하여서 객체지향 언어 만들기

--- src/oop1/MusicPlayerMain3.java
+++ src/oop1/MusicPlayerMain3.java
@@ -1,35 +1,20 @@
 package oop1;
 
-public class MusicPlayerMain2 {
+public class MusicPlayerMain3 {
     public static void main(String[] args) {
         MusicPlayerData data = new MusicPlayerData();
 
         //음악 플레이어 켜기
-        data.isOn = true;
-        System.out.println("음악 플레이어를 시작합니다.");
+        on(data);
 
-        //볼륨 증가
-        data.volume++;
-        System.out.println("음악 플레이어 볼륨:" + data.volume);
+        volumeUp(data);
+        volumeUp(data);
 
-        //볼륨 증가
-        data.volume++;
-        System.out.println("음악 플레이어 볼륨:"+data.volume);
+        volumeDown(data);
 
-        //볼륨 감소
-        data.volume--;
-        System.out.println("음악 플레이어 볼륨:"+data.volume);
+        showStatus(data);
 
-        //음악 플레이어 상태
-        System.out.println("음악 플레이어 상태 확인");
-        if(data.isOn){
-            System.out.println("음악 플레이어 on, 볼륨:"+data.volume);
-        }else {
-            System.out.println("음악 플레이어 OFF");
-        }
-
-        data.isOn = false;
-        System.out.println("음악 플레이어 종료");
+        off(data);
     }
     static void on(MusicPlayerData data){
         data.isOn = true;
@@ -37,7 +22,24 @@ public class MusicPlayerMain2 {
     }
     static void off(MusicPlayerData data){
         data.isOn = false;
-        System.out.println("음악 플레이어를 .");
+        System.out.println("음악 플레이어를 종료합니다.");
+    }
+
+    static void volumeUp(MusicPlayerData data){
+        data.volume++;
+        System.out.println("음악 플레이어 볼륨:" + data.volume);
+    }
+    static void volumeDown(MusicPlayerData data){
+        data.volume--;
+        System.out.println("음악 플레이어 볼륨:"+data.volume);
+    }
+    static void showStatus(MusicPlayerData data){
+        System.out.println("음악 플레이어 상태 확인");
+        if(data.isOn){
+            System.out.println("음악 플레이어 on, 볼륨:"+data.volume);
+        }else {
+            System.out.println("음악 플레이어 OFF");
+        }
     }
 }
 
---
+++ src/oop1/ValueData.java
@@ -0,0 +1,10 @@
+package oop1;
+
+public class ValueData {
+    int value;
+
+    void add(){
+        value++;
+        System.out.println("숫자 증가 value="+value); //메서드는 객체를 생성해야 호출 가능하다.
+    }
+}
