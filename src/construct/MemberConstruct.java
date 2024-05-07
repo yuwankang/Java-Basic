2ad91adfc4604091592450196efada4a8a1b3f6e
yuwankang <kangyuwan@naver.com>
2024-05-07 22:54:54 +0900

    this() 기능을 사용하면 생성자 내부에서 자신의 생성자를 호출할 수 있다. this() 생성자 코드의 첫줄에만 작성할 수 있다.

--- src/construct/MemberConstruct.java
+++ src/construct/MemberConstruct.java
@@ -5,6 +5,12 @@ public class MemberConstruct {
     int age;
     int grade;
 
+    //추가
+    MemberConstruct(String name, int age){
+        this(name,age, 50);//this() 기능을 사용하면 생성자 내부에서 자신의 생성자를 호출할 수 있다.
+        //this() 생성자 코드의 첫줄에만 작성할 수 있다.
+    }
+
     MemberConstruct(String name, int age, int grade){//생성자의 이름은 클래스와 같아야한다. 반환타입이 없다.
         System.out.println("생성자 호출 name="+name+",age="+age+",grade="+grade);
         this.name = name;
