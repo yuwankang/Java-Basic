587e3ac0925ef8cb821a4459f437c3e5803fc983
yuwankang <kangyuwan@naver.com>
2024-05-08 23:29:35 +0900

    같은 이름의 클래스가 있다면 import는 둘중 하나만 선택할 수 있다. 이때 자주 사용하는 클래스를 import하고 나머지 패키지를 포함한 전체 경로를 적어주면 된다. 둘다 전체 경로를 적어준다면 import는 사용하지 않아도 된다.

---
+++ src/pack/PackageMain2.java
@@ -0,0 +1,10 @@
+package pack;
+
+import pack.a.*; //패키지 모든 클래스를 생략하고 사용 가능
+public class PackageMain2 {
+    public static void main(String[] args) {
+        Data data = new Data();
+        User user = new User();
+        User2 user2 = new User2();
+    }
+}
